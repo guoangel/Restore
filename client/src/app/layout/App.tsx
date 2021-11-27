8dc3fff39f13dc5c269a079efb9eef1ad04b2aef
angel <fsguoangel@hotmail.com>
2021-11-27 23:58:52 +0800

    section9

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,7 +1,10 @@
 import { Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
-import { useEffect, useState } from "react";
-import { Route } from "react-router";
-import { Routes } from "react-router-dom";
+import { useCallback, useEffect, useState } from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import AboutPage from "../../features/about/AboutPage";
 import Catalog from "../../features/catalog/Catalog";
@@ -12,28 +15,30 @@ import Header from "./Header";
 import 'react-toastify/dist/ReactToastify.css';
 import NotFound from "../errors/NotFound";
 import BasketPage from "../../features/basket/BasketPage";
-import { getCookie } from "../util/util";
-import agent from "../api/agent";
 import LoadingComponent from "./LoadingComponent";
 import CheckoutPage from "../../features/checkout/CheckoutPage";
 import { useAppDispatch } from "../store/configureStore";
-import { setBasket } from "../../features/basket/basketSlice";
+import { fetchBasketAsync } from "../../features/basket/basketSlice";
+import Login from "../../features/account/Login";
+import Register from "../../features/account/Register";
+import { fetchCurrentUser } from "../../features/account/accountSlice";
 
 function App() {
   const dispatch = useAppDispatch();
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const buyerId = getCookie('buyerId');
-    if (buyerId) {
-      agent.Basket.get()
-        .then(basket => dispatch(setBasket(basket)))
-        .catch(error => console.log(error))
-        .finally(() => setLoading(false))
-    } else {
-      setLoading(false);
+  const initApp = useCallback(async () => {
+    try {
+      await dispatch(fetchCurrentUser());
+      await dispatch(fetchBasketAsync());
+    } catch (error) {
+      console.log(error);
     }
   }, [dispatch])
+
+  useEffect(() => {
+    initApp().then(() => setLoading(false));
+  }, [initApp])
 
   const [darkMode, setDarkMode] = useState(false);
   const paletteType = darkMode ? 'dark' : 'light'
@@ -52,7 +57,6 @@ function App() {
 
   if (loading) return <LoadingComponent message='Initialising app...' />
 
-
   return (
     <ThemeProvider theme={theme}>
       <ToastContainer position='bottom-right' hideProgressBar theme='colored' />
@@ -67,6 +71,8 @@ function App() {
         <Route path='/contact' element={<ContactPage />} />
         <Route path='/basket' element={<BasketPage />} />
         <Route path='/checkout' element={<CheckoutPage />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
         <Route element={<NotFound />} />
       </Routes>
       </Container>
--- client/src/app/store/configureStore.ts
+++ client/src/app/store/configureStore.ts
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import { accountSlice } from "../../features/account/accountSlice";
 import { basketSlice } from "../../features/basket/basketSlice";
 import { catalogSlice } from "../../features/catalog/catalogSlice";
+import { counterSlice } from "../../features/contact/counterSlice";
 
 // export function configureStore() {
 //     return createStore(counterReducer);
@@ -9,8 +11,10 @@ import { catalogSlice } from "../../features/catalog/catalogSlice";
 
 export const store = configureStore({
 reducer: {
+        counter: counterSlice.reducer,
         basket: basketSlice.reducer,
         catalog: catalogSlice.reducer,
+        account: accountSlice.reducer
     }
 })
 
